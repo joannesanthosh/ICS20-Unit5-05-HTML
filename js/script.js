f262d4bd2350c0b1979d6b7934d0941588dc973f
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-05-04 16:47:07 +0000

    change 4 files

--- js/script.js
+++ js/script.js
@@ -9,22 +9,25 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-Unit6-01-PWA/sw.js", {
-    scope: "/ICS20-Unit6-01-PWA/",
+  navigator.serviceWorker.register("/ICS20-Unit5-05-HTML/sw.js", {
+    scope: "/ICS20-Unit5-05-HTML/",
   })
 }
 
 /**
- * This function converts the degrees from fahrenheit to celsius
+ * Input
  */
 function convert() {
-  // input
-  const fahrenheit = parseInt(document.getElementById("fahrenheit").value)
+  var sideA = document.getElementById("side-a").value
+  var sideB = document.getElementById("side-b").value
+  var sideC = document.getElementById("side-c").value
 
   // process
-  const celsius = ((fahrenheit - 32) * 5) / 9
-
-  // output
-  document.getElementById("celsius").innerHTML =
-    "<p>The  temperature in celsius is: " + celsius.toFixed(2) + "°<p>"
-}
+  if (sideA == sideB == sideC) {
+    document.getElementById("answer").innerHTML = "The triangle is equilateral."
+  } else (sideA == sideB) || (sideA == sideC) || (sideB == sideC) {
+    document.getElementById("answer").innerHTML = "The triangle is isosceles."
+  } else (sideA <> sideB <> sideC) {
+    document.getElementById("answer").innerHTML = "The triangle is scalene."
+  }
+  
